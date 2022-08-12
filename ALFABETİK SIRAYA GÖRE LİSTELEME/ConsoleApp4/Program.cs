27fec777acb76d3848a6e0c18ae9cce119bc47fc
yunusyiilmaz <yns.ylmz1381@gmail.com>
2022-08-12 18:06:56 +0300

    Sıralama programı
    Sıralama programı

---
+++ ALFABETİK SIRAYA GÖRE LİSTELEME/ConsoleApp4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kaç adet veri istersiniz :");
+            int veri =Convert.ToInt32(Console.ReadLine());
+            string[] names=new string[veri];
+            for(int i = 0; i < names.Length; i++)
+            {
+                Console.Write("İSİM GİRİNİZ :");
+                string ad = Console.ReadLine();
+                names[i] = ad;
+            }
+            Console.WriteLine();
+            Console.WriteLine("NORMAL SIRALANMIŞ HALİ");
+            Console.WriteLine();
+            foreach(string liste in names)
+            {
+                Console.WriteLine(liste);
+            }
+            Console.WriteLine();
+            Console.WriteLine("ŞİMDİDE ALFABETİK SIRAYA GÖRE YAPACAĞIZ.");
+            Console.WriteLine();
+            Array.Sort(names);
+            foreach(string liste in names)
+            {
+                Console.WriteLine(liste);
+            }
+            Console.WriteLine();
+            Console.WriteLine("ŞİMDİ İSE TERSTEN SIRALANMIŞ HALİ");
+            Console.WriteLine();
+            Array.Reverse(names);
+            foreach(string liste in names)
+            {
+                Console.WriteLine(liste);
+            }
+            Console.ReadLine();
+        }
+    }
+}
